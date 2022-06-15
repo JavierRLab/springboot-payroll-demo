a7854c9546fc272d35ed9b3205f32565ae4b12d3
Javier Raez Labrado <jrlabra2@gmail.com>
2022-06-15 13:26:44 +0200

    employees hateoas

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,9 +34,15 @@ public class EmployeeController {
 	// Aggregate root
 	// tag::get-aggregate-root[]
 	@GetMapping("/employees")
-	List<Employee> all() {
-		log.debug("GET /employees");
-		return repository.findAll();
+	CollectionModel<EntityModel<Employee>> all() {
+		List<EntityModel<Employee>> employees = repository.findAll().stream()
+				.map(employee -> EntityModel.of(employee,
+						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+				.collect(Collectors.toList());
+
+		return CollectionModel.of(employees,
+				linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
 	}
 	// end::get-aggregate-root[]
 
