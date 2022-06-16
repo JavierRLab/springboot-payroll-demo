7a7eb336b9d4825eafc639949d1478c0c5334ce3
Javier Raez Labrado <jrlabra2@gmail.com>
2022-06-16 20:24:58 +0200

    cancel complete endpoints

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -9,15 +9,22 @@ import java.util.stream.Collectors;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mediatype.problem.Problem;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Order;
 import com.example.demo.entities.OrderModelAssembler;
+import com.example.demo.entities.Status;
 import com.example.demo.exceptions.OrderNotFoundException;
 import com.example.demo.repositories.OrderRepository;
 
@@ -64,24 +71,41 @@ public class OrderController {
 		return assembler.toModel(Order);
 	}
 
-	// @PutMapping("/orders/{id}")
-	// Order replaceOrder(@RequestBody Order newOrder, @PathVariable Long id) {
-	//
-	// return repository.findById(id)
-	// .map(Order -> {
-	// Order.setName(newOrder.getName());
-	// Order.setRole(newOrder.getRole());
-	// return repository.save(Order);
-	// })
-	// .orElseGet(() -> {
-	// newOrder.setId(id);
-	// return repository.save(newOrder);
-	// });
-	// }
+	@PutMapping("/orders/{id}/complete")
+	ResponseEntity<?> complete(@PathVariable Long id) {
 
-	// @DeleteMapping("/orders/{id}")
-	// ResponseEntity<?> deleteOrder(@PathVariable Long id) {
-	// repository.deleteById(id);
-	// return ResponseEntity.noContent().build();
-	// }
+		Order order = repository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException(id));
+
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.COMPLETED);
+			return ResponseEntity.ok(assembler.toModel(repository.save(order)));
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+				.body(Problem.create()
+						.withTitle("Method not allowed")
+						.withDetail("You can't complete an order that is in the status " + order.getStatus()));
+	}
+
+	@DeleteMapping("/orders/{id}/cancel")
+	ResponseEntity<?> cancel(@PathVariable Long id) {
+
+		Order order = repository.findById(id)
+				.orElseThrow(() -> new OrderNotFoundException(id));
+
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			order.setStatus(Status.CANCELLED);
+			return ResponseEntity.ok(assembler.toModel(repository.save(order)));
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.METHOD_NOT_ALLOWED)
+				.header(HttpHeaders.CONTENT_TYPE, MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE)
+				.body(Problem.create()
+						.withTitle("Method not allowed")
+						.withDetail("You can't cancel an order that is in the status " + order.getStatus()));
+	}
 }
